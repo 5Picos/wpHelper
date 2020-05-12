44622ac794ed3067cff54d078760df6dce7c5366
geramarenco@yahoo.com <geramarenco@yahoo.com>
2020-05-12 10:38:22 -0300

    version a prodccion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,78 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public numero: number = undefined;
+  private subscription: any;
 
-  constructor() {}
+  constructor(private toastController: ToastController, private platform: Platform) {}
 
+  ionViewDidEnter(){
+    this.subscription = this.platform.backButton.subscribe(()=>{
+      navigator['app'].exitApp();
+    });
+  }
+
+  ionViewWillLeave(){
+    this.subscription.unsubscribe();
+  }
+
+  async enviar(){
+    let t;
+    let n: string = this.numero.toString();
+
+    if (isNaN(this.numero)) {
+      // No ingreso algo numerico
+      t = await this.toastController.create({
+        message: "Ingrese un número",
+        duration: 2000,
+        position: "middle",
+      });
+      t.present();
+      return 0;
+    }
+
+    if (n.length == 10 && n[0] == "9") {
+      // Ingreso un numero con el 9 pero no es valido porque tiene lng = 10
+      t = await this.toastController.create({
+        message: "Numero incorrecto: falta un número",
+        duration: 2000,
+        position: "middle",
+      });
+      t.present();
+      return 0;
+    }
+
+    if (n.length == 11 && n[0] != "9") {
+      // Ingreso un numero sin el 9 pero no es valido porque tiene lng = 11
+      t = await this.toastController.create({
+        message: "Numero incorrecto: sobra un número",
+        duration: 2000,
+        position: "middle",
+      });
+      t.present();
+      return 0;
+    }
+
+    if (n.length != 11 && n.length != 10) {
+      // Ingreso un numero que no es un telefono
+      t = await this.toastController.create({
+        message: "Numero incorrecto: no es un teléfono",
+        duration: 2000,
+        position: "middle",
+      });
+      t.present();
+      return 0;
+
+    }
+
+    window.open("https://api.whatsapp.com/send?phone=" + this.getCountryCode() + this.numero);
+  }
+
+  private getCountryCode(): string {
+    return "54";
+  }
+
+  exit() {
+    navigator['app'].exitApp();
+  }
 }
